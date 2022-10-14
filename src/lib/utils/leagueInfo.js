4b0b342d2aeeb517124c4d10a60f7732651420c1
Iconstant100 <115602283+Iconstant100@users.noreply.github.com>
2022-10-14 10:07:52 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "863634326343004160"; // your league ID
+export const leagueID = "863634326343004160"; // your league ID  
 export const leagueName = "Paso Wine Cowboys"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
